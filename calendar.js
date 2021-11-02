6b3a722df4bf425056996448d996252bfba9d3ab
Makoto Onoue <like_a_newborn_baby@icloud.com>
2021-11-02 10:42:59 +0900

    change: from function base to object oriented way

--- calendar.js
+++ calendar.js
@@ -1,47 +1,75 @@
 'use strict'
 
-// {
-//
-// }
-// class MonthYearLine {
-//   constructor(date) {
-//     this.data = makeMonthYearLine(date)
-//   }
-// }
-function makeMonthYearLine(date) {
-  const head_spaces = makeHeadSpaces(date)
-  const month_year_str = makeMonthYearStr(date)
-  return require('printf')('% -22s', head_spaces + month_year_str)
+{
+  class MonthYearLine {
+    constructor(date) {
+      this.data = this.makeMonthYearLine(date)
+    }
+
+    makeMonthYearLine(date) {
+      const head_spaces = this.makeHeadSpaces(date)
+      const month_year_str = this.makeMonthYearStr(date)
+      return require('printf')('% -22s', head_spaces + month_year_str)
+    }
+
+    makeMonthYearStr(date) {
+      return `${date.month}月 ${date.year}`
+    }
+
+    calcHeadSpaces(date) {
+      const yearLen = date.year.toString().length
+      const monthLen = date.month.toString().length
+      return Math.ceil((17 - (yearLen + monthLen)) / 2)
+    }
+
+    makeHeadSpaces(year, month) {
+      return ' '.repeat(this.calcHeadSpaces(year, month))
+    }
+  }
+
+  class getDate {
+    constructor() {
+      const date = new Date()
+      const argv = require('minimist')(process.argv.slice(2))
+      this.year = this.getYear(date, argv)
+      this.month = this.getMonth(date, argv)
+    }
+
+    getYear(date, argv) {
+      return this.isUndefined(argv.y) ? date.getFullYear() : parseInt(argv.y)
+    }
+
+    getMonth(date, argv) {
+      return this.isUndefined(argv.m) ? date.getMonth() + 1 : parseInt(argv.m)
+    }
+
+    isUndefined(x) {
+      return typeof x === 'undefined'
+    }
+  }
+
+  // class DaysOfWeek {
+  //   constructor() {
+  //     this.data = ''
+  //   }
+  //
+  // }
+
+  // const isUndefined = x => typeof x === "undefined"
+  // function getDate () {
+  //   const year = isUndefined(argv.y) ? date.getFullYear() : parseInt(argv.y)
+  //   const month = isUndefined(argv.m) ? date.getMonth() + 1 : parseInt(argv.m)
+  //   return {year: year, month: month}
+  // }
+
+  module.exports = {getDate: getDate, MonthYearLine: MonthYearLine}
 }
 
-function makeMonthYearStr(date) {
-  return `${date.month}月 ${date.year}`
-}
 
-function calcHeadSpaces(date) {
-  const yearLen = date.year.toString().length
-  const monthLen = date.month.toString().length
-  return Math.ceil((17 - (yearLen + monthLen)) / 2)
-}
 
-function makeHeadSpaces(year, month) {
-  return ' '.repeat(calcHeadSpaces(year, month))
-}
-
-const isUndefined = x => typeof x === "undefined"
-
-function getDate () {
-  const argv = require('minimist')(process.argv.slice(2))
-  const d = new Date()
-  const year = isUndefined(argv.y) ? d.getFullYear() : parseInt(argv.y)
-  const month = isUndefined(argv.m) ? d.getMonth() + 1 : parseInt(argv.m)
-  return {year: year, month: month}
-}
 
 function main() {
-  const date = getDate()
-  const h = makeMonthYearLine(date)
+  // const date = getDate()
+  // const h = makeMonthYearLine(date)
   console.log(h)
 }
-
-module.exports = {makeMonthYearLine: makeMonthYearLine, get_date: getDate}
--- calendar.test.js
+++ calendar.test.js
@@ -1,24 +1,24 @@
 const { execSync } = require('child_process')
 
 describe('makeMonthYearLine()', () => {
-  const {makeMonthYearLine, get_date} = require('./calendar');
+  const {getDate, MonthYearLine} = require('./calendar');
 
   describe('without arguments', () => {
     test('returns the current month and year', () => {
       const stdout = execSync('cal')
       const monthYearLine = stdout.toString().split("\n")[0]
-      const date = get_date()
-      expect(makeMonthYearLine(date)).toBe(monthYearLine);
+      const date = new getDate()
+      expect(new MonthYearLine(date).data).toBe(monthYearLine);
     })
   })
 
   describe('with arguments "-m 1"', () => {
-    test('returns "1月 XXXX(= the current year)"', () => {
+    test('returns "1月 YYYY(= the current year)"', () => {
       const stdout = execSync('cal -m 1')
       const monthYearLine = stdout.toString().split("\n")[0]
-      const date = get_date()
+      const date = new getDate()
       date.month = 1
-      expect(makeMonthYearLine(date)).toBe(monthYearLine);
+      expect(new MonthYearLine(date).data).toBe(monthYearLine);
     })
   })
 })
