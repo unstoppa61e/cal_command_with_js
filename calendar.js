dfb0568fe9c1b71ec9b29d271b832fd564828933
Makoto Onoue <like_a_newborn_baby@icloud.com>
2021-11-02 14:27:50 +0900

    change: from printf to handmade code

--- calendar.js
+++ calendar.js
@@ -2,70 +2,72 @@
 
 {
   class MonthYearLine {
-    constructor(date) {
+    constructor (date) {
       this.data = this.makeMonthYearLine(date)
     }
 
-    makeMonthYearLine(date) {
-      const head_spaces = this.makeHeadSpaces(date)
-      const month_year_str = this.makeMonthYearStr(date)
-      return require('printf')('% -22s', head_spaces + month_year_str)
+    makeMonthYearLine (date) {
+      const lineLength = 22
+      const headSpaces = this.makeHeadSpaces(date)
+      const monthYearStr = this.makeMonthYearStr(date)
+      const tailSpaces = ' '.repeat(lineLength - (headSpaces + monthYearStr).length)
+      return headSpaces + monthYearStr + tailSpaces
     }
 
-    makeMonthYearStr(date) {
+    makeMonthYearStr (date) {
       return `${date.month}月 ${date.year}`
     }
 
-    calcHeadSpaces(date) {
+    calcHeadSpaces (date) {
       const yearLen = date.year.toString().length
       const monthLen = date.month.toString().length
       return Math.ceil((17 - (yearLen + monthLen)) / 2)
     }
 
-    makeHeadSpaces(year, month) {
+    makeHeadSpaces (year, month) {
       return ' '.repeat(this.calcHeadSpaces(year, month))
     }
   }
 
-  class getDate {
-    constructor() {
+  class GetDate {
+    constructor () {
       const date = new Date()
       const argv = require('minimist')(process.argv.slice(2))
       this.year = this.getYear(date, argv)
       this.month = this.getMonth(date, argv)
     }
 
-    getYear(date, argv) {
+    getYear (date, argv) {
       return this.isUndefined(argv.y) ? date.getFullYear() : parseInt(argv.y)
     }
 
-    getMonth(date, argv) {
+    getMonth (date, argv) {
       return this.isUndefined(argv.m) ? date.getMonth() + 1 : parseInt(argv.m)
     }
 
-    isUndefined(x) {
+    isUndefined (x) {
       return typeof x === 'undefined'
     }
   }
 
   class DaysOfWeekLine {
-    constructor() {
+    constructor () {
       this.data = '日 月 火 水 木 金 土  '
     }
   }
 
   class DatesBlock {
-    constructor(yearMonth) {
+    constructor (yearMonth) {
       this.data = this.makeDatesBlock(yearMonth)
     }
 
-    makeDatesBlock(yearMonth) {
+    makeDatesBlock (yearMonth) {
       const datesStrs = this.getDatesStrs(yearMonth)
       const weeksStrs = this.makeWeeksStrs(datesStrs)
       return weeksStrs.join('\n')
     }
 
-    makeWeeksStrs(datesStrs) {
+    makeWeeksStrs (datesStrs) {
       const weeksStrs = []
       for (let i = 0; i < 6; i++) {
         const weekHeadIndex = i * 7
@@ -75,14 +77,15 @@
       return weeksStrs
     }
 
-    getDatesStrs(yearMonth) {
+    getDatesStrs (yearMonth) {
       const datesStrs = []
       const date = this.getFirstDay(yearMonth)
       for (let i = 0; i < date.getDay(); i++) {
         datesStrs.push(' '.repeat(2))
       }
       for (; date.getMonth() + 1 === yearMonth.month; date.setDate(date.getDate() + 1)) {
-        datesStrs.push(require('printf')('% 2s', date.getDate().toString()))
+        const dateStr = date.getDate().toString()
+        datesStrs.push(' '.repeat(2 - dateStr.length) + dateStr)
       }
       while (datesStrs.length < 7 * 6) {
         datesStrs.push(' '.repeat(2))
@@ -100,12 +103,12 @@
   }
 
   class Calendar {
-    constructor() {
+    show () {
       console.log(this.makeCalendar())
     }
 
-    makeCalendar() {
-      const date = new getDate()
+    makeCalendar () {
+      const date = new GetDate()
       const monthYearLine = new MonthYearLine(date)
       const daysOfWeekLine = new DaysOfWeekLine()
       const datesBlock = new DatesBlock(date)
@@ -113,7 +116,8 @@
     }
   }
 
-  new Calendar()
+  const calendar = new Calendar()
+  calendar.show()
 
-  module.exports = {getDate: getDate, MonthYearLine: MonthYearLine, DaysOfWeek: DaysOfWeekLine, DatesBlock: DatesBlock, Calendar: Calendar}
+  module.exports = { GetDate: GetDate, MonthYearLine: MonthYearLine, DaysOfWeek: DaysOfWeekLine, DatesBlock: DatesBlock, Calendar: Calendar }
 }
--- calendar.test.js
+++ calendar.test.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process')
-const {getDate: getDate, MonthYearLine: MonthYearLine, DaysOfWeek: DaysOfWeekLine, DatesBlock: DatesBlock, Calendar: Calendar} = require('./calendar')
-function toZsh(str) {
+const { GetDate, MonthYearLine, DaysOfWeek: DaysOfWeekLine, DatesBlock } = require('./calendar')
+function toZsh (str) {
   const lines = str.split('\n')
   return lines.slice(0, lines.length - 1).join('\n')
 }
@@ -9,8 +9,8 @@ describe('makeMonthYearLine', () => {
   describe('without arguments', () => {
     test('returns the current month and year', () => {
       const stdout = execSync('cal')
-      const monthYearLine = stdout.toString().split("\n")[0]
-      const date = new getDate()
+      const monthYearLine = stdout.toString().split('\n')[0]
+      const date = new GetDate()
       expect(new MonthYearLine(date).data).toBe(monthYearLine)
     })
   })
@@ -19,7 +19,7 @@ describe('makeMonthYearLine', () => {
     test('returns "1月 YYYY(= the current year)"', () => {
       const stdout = execSync('cal -m 1')
       const monthYearLine = stdout.toString().split('\n')[0]
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 1
       expect(new MonthYearLine(date).data).toBe(monthYearLine)
     })
@@ -37,7 +37,7 @@ describe('DatesBlock', () => {
     test('returns the block of the current month', () => {
       const stdout = execSync('cal -m 1')
       const datesBlock = toZsh(stdout.toString().split('\n').slice(2).join('\n'))
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 1
       expect(new DatesBlock(date).data).toBe(datesBlock)
     })
@@ -47,7 +47,7 @@ describe('DatesBlock', () => {
     test('returns the block of the current month', () => {
       const stdout = execSync('cal -m 2')
       const datesBlock = toZsh(stdout.toString().split('\n').slice(2).join('\n'))
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 2
       expect(new DatesBlock(date).data).toBe(datesBlock)
     })
@@ -57,7 +57,7 @@ describe('DatesBlock', () => {
     test('returns the block of the current month', () => {
       const stdout = execSync('cal -m 3')
       const datesBlock = toZsh(stdout.toString().split('\n').slice(2).join('\n'))
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 3
       expect(new DatesBlock(date).data).toBe(datesBlock)
     })
@@ -67,7 +67,7 @@ describe('DatesBlock', () => {
     test('returns the block of the current month', () => {
       const stdout = execSync('cal -m 4')
       const datesBlock = toZsh(stdout.toString().split('\n').slice(2).join('\n'))
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 4
       expect(new DatesBlock(date).data).toBe(datesBlock)
     })
@@ -77,7 +77,7 @@ describe('DatesBlock', () => {
     test('returns the block of the current month', () => {
       const stdout = execSync('cal -m 5')
       const datesBlock = toZsh(stdout.toString().split('\n').slice(2).join('\n'))
-      const date = new getDate()
+      const date = new GetDate()
       date.month = 5
       expect(new DatesBlock(date).data).toBe(datesBlock)
     })
